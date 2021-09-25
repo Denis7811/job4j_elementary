95cb1ea0b06cbcdeed5213c68d9fcf061a8b288f
Denis7811 <zz926@ya.ru>
2021-09-25 13:28:37 +0300

    Только Хелло без возраста

---
+++ src/main/java/ru/job4j/ArgMethod1.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class ArgMethod1 {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        ArgMethod1.hello(name);
+        ArgMethod1.hello(name);
+        ArgMethod1.hello(name);
+        ArgMethod1.hello(name);
+    }
+}
