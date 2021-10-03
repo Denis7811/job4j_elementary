135521108ea081ccca806436ecc050abc347551c
Denis7811 <zz926@ya.ru>
2021-10-03 11:20:05 +0300

    SwitchWeek1

--- src/main/java/ru/job4j/SqArea.java
+++ src/main/java/ru/job4j/SqArea.java
@@ -9,7 +9,7 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(2, 2);
+        double result1 = SqArea.square(1, 1);
         System.out.println(" p = 1, k = 1, s = 2, real = " + result1);
     }
 }
