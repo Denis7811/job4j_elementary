acebcaa2b72fc0209f7488ea1a6d52d5d2ae00b1
Denis7811 <zz926@ya.ru>
2021-10-04 06:45:58 +0300

    Counter 1

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,8 +3,7 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        int i;
-        for (i = start; i <= finish; i++) {
+        for (int i = start; i <= finish; i++) {
             sum = sum + i;
         }
         return sum;
@@ -12,8 +11,7 @@ public class Counter {
 
     public static int sumByEven(int start, int finish) {
         int sum1 = 0;
-        int a;
-        for (a = start; a <= finish; a++) {
+        for (int a = start; a <= finish; a++) {
             if ((a % 2) == 0) {
                 sum1 = sum1 + a;
             }
