1ee4520ac97a458ff43dfecc9fa252326d2af435
Denis7811 <zz926@ya.ru>
2021-10-10 10:50:49 +0300

    square1

--- src/main/java/array/Square.java
+++ src/main/java/array/Square.java
@@ -11,8 +11,8 @@ public class Square {
 
     public static void main(String[] args) {
         int[] array = calculate(5);
-        for (int index : array) {
-            System.out.println(index);
+        for (int i : array) {
+            System.out.println(i);
         }
     }
 }
