14956f77cc098334470a529831c4bc0f23324e5b
Denis7811 <zz926@ya.ru>
2021-10-14 05:46:10 +0300

    6.5.3. Сортировка выборкой

--- src/main/java/array/SortSelected.java
+++ src/main/java/array/SortSelected.java
@@ -4,9 +4,9 @@ public class SortSelected {
 
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, i, ((data.length) - 1));
-            int index = FindLoop.indexOf(data, min, i, ((data.length) - 1));
-            int[] arrayFinal = SwitchArray.swap(data, i, index);
+            int min = MinDiapason.findMin(data, i, (data.length - 1));
+            int index = FindLoop.indexOf(data, min, i, (data.length - 1));
+            SwitchArray.swap(data, i, index);
         }
         return data;
     }
