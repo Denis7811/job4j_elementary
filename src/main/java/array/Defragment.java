0a9e89b64e6c50c05b7608b33f5445bbfa89fbad
Denis7811 <zz926@ya.ru>
2021-10-15 06:58:13 +0300

    6.8. Дефрагментация массива

---
+++ src/main/java/array/Defragment.java
@@ -0,0 +1,28 @@
+package array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                for (int i = index+1; i < array.length; i++) {
+                    if (array[i] != null) {
+                        int notNullIndex = i;
+                        SwitchArray.swapString(array, point, notNullIndex);
+                    }
+                }
+                System.out.print(array[index] + " ");
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
