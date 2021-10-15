c4ac7545cdac9d702ec5374a1f43829288f2d7b3
Denis7811 <zz926@ya.ru>
2021-10-15 05:36:39 +0300

    6.6.8. Двухмерный массив. Таблица умножения

--- src/main/java/array/Matrix.java
+++ src/main/java/array/Matrix.java
@@ -5,7 +5,7 @@ public class Matrix {
         int[][] array = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                array[i][j] = (i + 1) * (j + 1);
+                 array[i][j] = (i + 1) * (j + 1);
             }
         }
         return array;
