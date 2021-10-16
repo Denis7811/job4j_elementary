91b189294e72532b68c63e513a7dda2c169ed67f
Denis7811 <zz926@ya.ru>
2021-10-16 08:34:29 +0300

    6.8. Дефрагментация массива

--- src/main/java/array/Defragment.java
+++ src/main/java/array/Defragment.java
@@ -5,7 +5,7 @@ public class Defragment {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
                 int point = index;
-                for (int i = index + 1; i < array.length; i++) {
+                for (int i = (index + 1); i < array.length; i++) {
                     if (array[i] != null) {
                         int notNullIndex = i;
                         SwitchArray.swapString(array, point, notNullIndex);
---
+++ src/main/java/array/Machine.java
@@ -0,0 +1,11 @@
+package array;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[100];
+        int size = 0;
+        return Arrays.copyOf(rsl, size);
+    }
+}
+}
